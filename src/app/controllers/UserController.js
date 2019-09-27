6bc566244773f5f46e40be8c508c51594368f920
greisonsantos <greisonsantos03@gmail.com>
2019-09-27 13:44:40 -0300

    criptografia de senha

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,12 +1,13 @@
 const { User } = require("../models");
 class UserController {
   create(req, res) {
-    return res.render("auth/singup");
+    return res.render("auth/signup");
   }
 
   async store(req, res) {
-    await User.create(req.body);
+    req.body.avatar = "teste.jpg";
 
+    await User.create(req.body);
     return res.redirect("/");
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,9 @@
 const express = require("express");
 const routes = express.Router();
 
-// const UserController = require("./app/controllers/UserController");
+const UserController = require("./app/controllers/UserController");
 
-
-routes.get("/",(req, res)=>{
-  res.render("auth/singup");
-});
-
-
-// routes.post("/signup", UserController.store);
+routes.get("/signup", UserController.create);
+routes.post("/signup", UserController.store);
 
 module.exports = routes;
